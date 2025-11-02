4506985cc640b681dd36f614665e2c860ffb3551
DeltaCream <81692797+DeltaCream@users.noreply.github.com>
2025-11-03 01:16:24 +0800

    added search icon for search bar in command menu

--- components/command-menu.tsx
+++ components/command-menu.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { useRouter } from "next/navigation"
 import { Dialog, DialogTrigger, type DialogProps } from "@radix-ui/react-dialog"
-import { Circle, File, Laptop, Moon, Sun } from "lucide-react"
+import { Circle, File, Laptop, Moon, SearchIcon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { commandConfig } from "@/config/commands"
@@ -68,6 +68,7 @@ export function CommandMenu({ ...props }: DialogProps) {
         onClick={() => setOpen(true)}
         {...props}
       >
+        <SearchIcon />
         <span className="hidden lg:inline-flex">Search the website...</span>
         <span className="inline-flex lg:hidden">Search...</span>
         {/* <kbd className="pointer-events-none absolute right-[0.3rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
